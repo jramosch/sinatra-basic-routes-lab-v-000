9e805e8b0a8aa3a17aead24ca4f4bd38eb69c91f
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-01 23:49:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "Jamilya"
+  end
+
+  get '/hometown' do
+    "Brooklyn"
+  end
+
+  get 'favorite-song' do
+    "DNA by Kendrick Lamar"
+  end
 end
